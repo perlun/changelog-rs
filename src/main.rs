f46f4accf69f274b029ce4654fb0092d5127b560
Per Lundberg <perlun@gmail.com>
2017-07-07 10:40:00 +0300

    Default to current folder when run without arguments
    Fixes #12.

--- src/main.rs
+++ src/main.rs
@@ -14,22 +14,11 @@ use std::process::exit;
 fn main() {
     let args: Vec<_> = env::args().collect();
 
-    if args.len() == 2 {
-        let ref repository_path = args[1];
-        let git_tag_parser = GitTagParser {
-            repository_path: repository_path.clone()
-        };
-
-        let version_tag_pairs = git_tag_parser.get_version_tag_pairs();
-
-        for (from_tag, to_tag) in version_tag_pairs.into_iter() {
-            let generator = ChangelogGenerator {
-                repository_path: repository_path.clone(),
-                from_revision: from_tag,
-                to_revision: to_tag
-            };
-            generator.generate_changelog();
-        }
+    if args.len() == 1 {
+        generate_changelog_for_folder(&String::from("."));
+    }
+    else if args.len() == 2 {
+        generate_changelog_for_folder(&args[1]);
     } else if args.len() == 4 {
         let generator = ChangelogGenerator {
             repository_path: args[1].clone(),
@@ -44,5 +33,22 @@ fn main() {
         println!("If the 'from_revision' and 'to_revision' are not provided, a full log \
                   including all SemVer-tagged versions will be produced.");
         exit(1);
+    }
+}
+
+fn generate_changelog_for_folder(repository_path: &String) {
+    let git_tag_parser = GitTagParser {
+        repository_path: repository_path.clone()
+    };
+
+    let version_tag_pairs = git_tag_parser.get_version_tag_pairs();
+
+    for (from_tag, to_tag) in version_tag_pairs.into_iter() {
+        let generator = ChangelogGenerator {
+            repository_path: repository_path.clone(),
+            from_revision: from_tag,
+            to_revision: to_tag
+        };
+        generator.generate_changelog();
     }
 }
