cca8ecfbf91e087209d0fb27fdff0b8937abac9b
Per Lundberg <perlun@gmail.com>
2017-07-07 22:34:06 +0300

    Use clap for argument parsing
    Closes #13

--- src/main.rs
+++ src/main.rs
@@ -2,41 +2,50 @@
 #![deny(unused_variables)]
 
 extern crate semver;
+extern crate clap;
 
 mod changelog_generator;
 mod git_tag_parser;
 
+use clap::{App, Arg};
 use changelog_generator::ChangelogGenerator;
 use git_tag_parser::GitTagParser;
-use std::env;
-use std::process::exit;
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let matches = App::new("changelog-rs")
+                          .version("0.1.0")
+                          .author("Per Lundberg <pelrun@gmail.com>")
+                          .about("Trivial Rust-based CHANGELOG.md generation tool")
+                          .arg(Arg::with_name("REPOSITORY_PATH")
+                               .help("Sets the path of the repository to generate the changelog for. Defaults to the current directory.")
+                               .index(1))
+                          .arg(Arg::with_name("FROM_REVISION")
+                              .help("The starting revision (usually the previous SemVer version).")
+                              .index(2))
+                          .arg(Arg::with_name("TO_REVISION")
+                              .help("The ending revision (the current/latter SemVer version).")
+                              .index(3))
+                          .get_matches();
 
-    if args.len() == 1 {
-        generate_changelog_for_folder(&String::from("."));
-    }
-    else if args.len() == 2 {
-        generate_changelog_for_folder(&args[1]);
-    } else if args.len() == 4 {
+    let repository_path = String::from(matches.value_of("REPOSITORY_PATH").unwrap_or("."));
+
+    if matches.is_present("FROM_REVISION") && matches.is_present("TO_REVISION") {
+        let from_revision = String::from(matches.value_of("FROM_REVISION").unwrap());
+        let to_revision = String::from(matches.value_of("TO_REVISION").unwrap());
+
         let generator = ChangelogGenerator {
-            repository_path: args[1].clone(),
-            from_revision: args[2].clone(),
-            to_revision: args[3].clone()
+            repository_path: repository_path,
+            from_revision: from_revision,
+            to_revision: to_revision
         };
         generator.generate_changelog();
-    } else {
-        println!("Usage: {} <path> [<from_revision> <to_revision>]\n",
-                 args[0]);
-        println!("The path must be a clone of valid git repository.");
-        println!("If the 'from_revision' and 'to_revision' are not provided, a full log \
-                  including all SemVer-tagged versions will be produced.");
-        exit(1);
+    }
+    else {
+        generate_changelog_for_folder(repository_path);
     }
 }
 
-fn generate_changelog_for_folder(repository_path: &String) {
+fn generate_changelog_for_folder(repository_path: String) {
     let git_tag_parser = GitTagParser {
         repository_path: repository_path.clone()
     };
